e3f93d4a5dbc5295825b310782ec4fbcde04b81c
Matthew Thomas <matthew.thomas12@okstate.edu>
2026-02-03 12:47:19 -0600

    Add project files.

---
+++ MT Basic App GitHub/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading;
+
+namespace MT_Basic_App_GitHub
+{
+	public class Class1
+	{
+		public string Name { get; }
+		public Class1()
+		{
+			Name = "[Matthew Thomas] - The Code Master"; // Insert your name and
+			//moniker
+		}
+	}
+}
---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,18 @@
+using MT_Basic_App_GitHub;
+using Xunit;
+namespace MT_Basic_App_GitHub.Tests
+{
+	public class UnitTest1
+	{
+		[Fact]
+		public void TestNameProperty()
+		{
+			// Arrange
+			Class1 tp = new Class1();
+			// Act
+			var result = tp.Name;
+			// Assert
+			Assert.Equal("[Matthew Thomas] - The Code Master", result);
+		}
+	}
+}
